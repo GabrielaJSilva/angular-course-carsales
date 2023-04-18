a7c2ef3f36b47ae64d4d2ce19a9e4cd645beae22
GabrielaJSilva <gabrielajsv@gmail.com>
2023-04-18 00:07:18 -0400

    Criando Reop

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   privateKey: '2f639018-7c65-44df-a3a2-a070bd2a10a8',
   // connectionUrlDatabase: 'mongodb://127.0.0.1:27017/car-sales',
-  connectionUrlDatabase: '',
+  connectionUrlDatabase: 'mongodb+srv://gabi:gabi@cluster0.4jb2xa0.mongodb.net/test',
   apiPort: 8080,
   corsOptions: {
     origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
